e264b586dc10fe3cc2268e4db092fc8c8839e0d1
Julien C <julien.castelain@liferay.com>
2017-01-26 08:28:57 +0100

    Adds a --namespace option
    Fixes #25

---
+++ test/expected/expected-namespace-define-change.es.js
@@ -0,0 +1,10 @@
+MyNameSpace.define("modal@2.0.0/js/namespace-define-change.es", ["exports"], function(exports) {
+
+		var foo = {
+			bar: function () {
+				return 'baz';
+			}
+		};
+
+    exports.default = foo;
+});
---
+++ test/expected/expected-namespace-define-override-config.js
@@ -0,0 +1,6 @@
+{
+    "modal@2.0.0/js/namespace-define-override.es": {
+        "dependencies": ["exports"],
+        "path": "modal@2.0.0/js/namespace-define-override.es.js"
+    }
+}
