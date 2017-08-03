00cf9ae8a42c030dda261e3b82f00ed1d57fc2ee
Robert-Frampton <ROBERT.FRAMPTON@LIFERAY.COM>
2017-08-03 11:04:41 -0700

    Add test for moduleConfig object

---
+++ test/expected/expected-module-config-object.js
@@ -0,0 +1,10 @@
+{
+    "package-name@1.0.0/js/address1.es": {
+        "dependencies": ["exports", "modal@1.0.0/js/address2.es"],
+        "path": "package-name@1.0.0/js/address1.es.js"
+    },
+    "package-name@1.0.0/js/address2.es": {
+        "dependencies": ["exports"],
+        "path": "package-name@1.0.0/js/address2.es.js"
+    }
+}
